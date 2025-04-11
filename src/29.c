d29abdd8f4c7237da3fb5078e4bd44cbf6a9c930
tamjosefaxon <tamjosefaxon@gmail.com>
2025-04-11 11:33:16 +0000

    Add 29.c

---
+++ src/29.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    if (x > 5) {
+        printf("The value of x is greater than 5.\n");
+    } else {
+        printf("The value of x is not greater than 5.\n");
+    }
+
+    return 0;
+}
